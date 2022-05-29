42cd0f98c46ae06982a11295721e82eb2d1227a4
elzoka <mahmoud_elzoka@yahoo.com>
2022-05-29 15:46:40 +0200

    UP-49 init error handler

--- apps/api/src/main.js
+++ apps/api/src/main.js
@@ -6,12 +6,15 @@
 import * as express from 'express';
 import config from '@/config';
 import logger from '@/logger';
+import error_handler from '@/middlewares/error_handler';
 
 const app = express();
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
+
+app.use(error_handler);
 
 const port = config.port || 3000;
 const server = app.listen(port, config.host, () => {
